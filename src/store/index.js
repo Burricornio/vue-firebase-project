c7cf763f471cbc428dbfda5cc68a795619cca5c3
Burricornio <alejandrocf.web@gmail.com>
2018-05-08 20:55:49 +0200

    Componentes administrador

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import authModule from '@/modules/auth'
+import userModule from '@/modules/user'
 
 // Utilizamos el plugin
 Vue.use(Vuex)
@@ -34,6 +35,7 @@ export default new Vuex.Store({
         }
     },
     modules: {
-        authModule
+        authModule,
+        userModule
     }
 })
